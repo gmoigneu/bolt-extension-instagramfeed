f05bf3ee28e0619025c5f617ff57eb4393898245
Guillaume Moigneu Niels <n@nls.io>
2015-08-09 09:16:06 +0200

    Handle wrong config & add phpdoc

--- Extension.php
+++ Extension.php
@@ -14,11 +14,9 @@ class Extension extends BaseExtension
 
     private $version = "v0.9.0";
 
-    public function __construct(Application $app)
-    {
-        parent::__construct($app);
-    }
-
+    /**
+     * Add the Twig function and template folder
+     */
     public function initialize() {
         if ($this->app['config']->getWhichEnd() == 'frontend') {
             // Twig hook
@@ -35,15 +33,26 @@ class Extension extends BaseExtension
 
     /**
      * Render the Twitter feed
+     *
+     * @param int $count
+     * @return \Twig_Markup
      */
     public function twigInstagramFeed($count = 10) {
 
         $images = $this->getImages($count);
 
-        $html = $this->app['render']->render('_instagram.twig', ['images' => $images]);
-        return new \Twig_Markup($html, 'UTF-8');
+        if(count($images)) {
+            $html = $this->app['render']->render('_instagram.twig', ['images' => $images]);
+            return new \Twig_Markup($html, 'UTF-8');
+        } else return null;
     }
 
+    /**
+     * Request Instagram account last images
+     *
+     * @param $count
+     * @return null|array
+     */
     protected function getImages($count)
     {
         $cacheId = 'instagramfeed_images_'.$count;
@@ -51,24 +60,33 @@ class Extension extends BaseExtension
         if ($this->app['cache']->contains($cacheId)) {
             return $this->app['cache']->fetch($cacheId);
         } else {
+
             $client = new Client();
 
             $implicitAuth = new ImplicitAuth($this->config);
             $client->getEmitter()->attach($implicitAuth);
 
             $oauth = $client->post('https://instagram.com/oauth/authorize');
-
             $access_token = $implicitAuth->getAccessToken();
 
-            $content = $client->get('https://api.instagram.com/v1/users/self/media/recent?count='.$count.'&access_token='.$access_token);
-            $body = json_decode($content->getBody());
-            $images = $body->data;
+            // Valid Access Token
+            if($access_token) {
+                $content = $client->get('https://api.instagram.com/v1/users/self/media/recent?count='.$count.'&access_token='.$access_token);
+                $body = json_decode($content->getBody());
+                $images = $body->data;
 
-            $this->app['cache']->save($cacheId, $images, $this->config['cache_lifetime']);
-            return $images;
+                $this->app['cache']->save($cacheId, $images, $this->config['cache_lifetime']);
+                return $images;
+            }
+
+            // Auth failed or empty account
+            return null;
         }
     }
 
+    /**
+     * @return array
+     */
     protected function getDefaultConfig()
     {
         return array(
@@ -81,9 +99,3 @@ class Extension extends BaseExtension
     }
 
 }
-
-
-
-
-
-
